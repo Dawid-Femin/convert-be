b26c93812351a488832d2139ea6394259188a4dd
Dawid-Femin <d.femin@wp.pl>
2026-03-19 23:10:02 +0100

    feat: add image conversion service

---
+++ src/conversion/conversion.service.ts
@@ -0,0 +1,17 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as sharp from 'sharp';
+import { OutputFormat } from './enums/image-format.enum';
+
+@Injectable()
+export class ConversionService {
+  async convert(
+    file: Express.Multer.File,
+    targetFormat: OutputFormat,
+  ): Promise<Buffer> {
+    try {
+      return await sharp(file.buffer).toFormat(targetFormat).toBuffer();
+    } catch {
+      throw new BadRequestException('Failed to convert image');
+    }
+  }
+}
